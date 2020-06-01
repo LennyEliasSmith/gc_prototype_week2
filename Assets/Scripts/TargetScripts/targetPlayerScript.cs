13353b7aa74a9863642a3a0dfef8414aea6fb268
Lenny Smith <lenny.elias.s@gmail.com>
2020-06-01 14:49:40 +0300

    Added Target Script (1/3)
    Added Basic Target Script functionality. Targets Activate and if a boolean is activated then they loop moving from one point to another

---
+++ Assets/Scripts/TargetScripts/targetPlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetPlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hrz = Input.GetAxis("Horizontal");
+        float vrt = Input.GetAxis("Vertical");
+
+        Vector3 mvmnt = new Vector3(hrz * speed,0 , vrt * speed);
+        rb.AddForce(mvmnt);
+    }
+}
---
+++ Assets/Scripts/TargetScripts/targetScript.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetScript : MonoBehaviour
+{
+    public Transform targetObj;
+    public Rigidbody2D targetRb;
+    private GameObject player;
+
+    public float speed = 2f;
+    public float distance = 2f;
+    public int range = 10;
+
+    public bool isTargetActive = false;
+    public bool isTargetMoving = false;
+
+    
+ 
+
+
+
+    void Start()
+    {
+        targetObj = GetComponent<Transform>();
+        targetRb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+          
+    }
+
+
+    void Update()
+    {
+        if(isTargetActive == false) { 
+
+        PlayerInDistanceOfTarget();
+        
+            
+
+        }
+
+        if (isTargetMoving && isTargetActive)
+        {
+
+            TargetMovement();
+        }
+
+    }
+
+    //Check if distance between player and target
+    public void PlayerInDistanceOfTarget()
+    {
+
+        Vector3 targetPos = targetObj.position;
+        Vector3 playerPos = player.transform.position;
+
+        if (Vector3.Distance(targetPos, playerPos) < range)
+        {
+          ActivateTarget();
+
+        }
+
+
+    }
+
+    
+    //Rotates Target up and sets boolean to true
+    public void ActivateTarget()
+    {
+        transform.Rotate(Vector3.right * 90 );
+
+        isTargetActive = true;
+
+        
+
+    }
+
+    //Moves target between two points
+    public void TargetMovement()
+    {
+        if (isTargetActive)
+        {
+            Debug.Log("target Move");
+
+            targetObj.position = new Vector3(Mathf.PingPong(Time.time * speed, distance ), transform.position.y, transform.position.z);
+
+        }
+
+    }
+}
