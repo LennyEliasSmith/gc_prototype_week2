24db19569f2fc9fc6eca410c88e44d1889052abb
Eero <Salmi>
2020-06-01 16:53:09 +0300

    added pistol script

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    public float damage;
+    public float range;
+    public float fireRate;
+    public float ammo;
+
+    private Camera playerCam;
+    private GameObject hitObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ammo > 0)
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+                Debug.Log("PewPew");
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 tDirection = playerCam.transform.forward;
+
+        ammo--;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerCam.transform.position, tDirection, out hit))
+        {
+            hitObject = hit.transform.gameObject;
+
+            Debug.Log(hitObject);
+
+            if (hitObject.CompareTag("Enemy"))
+            {
+                Destroy(hitObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TargetDummy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDummy : MonoBehaviour
+{
+
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage()
+    {
+        health--;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
