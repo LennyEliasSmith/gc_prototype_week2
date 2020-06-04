4bb09a23c59c568c16284debc0c6c2ce1c9b9579
Juhis-pc <chook-fish@hotmail.com>
2020-06-04 16:12:53 +0300

    Main menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string loadLevel;
+
+    public void startGame()
+    {
+        Application.LoadLevel(loadLevel);
+    }
+}
