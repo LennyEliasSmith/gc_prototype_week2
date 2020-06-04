333253100e1698d2baaa6e3eb2532a7b2d092d78
Luca Virjo <lucan.posti@gmail.com>
2020-06-04 12:09:26 +0300

    Timer
    Added a timer to the screen

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text txt;
+
+    private float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        float displayTime = Mathf.Round(time * 100f) / 100f;
+
+        txt.text = "Time: " + displayTime;
+    }
+}
